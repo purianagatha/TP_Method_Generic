79d4fa2e879976ee6d03d2075b80a96242a4e37a
purianagatha <purianagatha@gmail.com>
2024-03-18 04:20:14 +0700

    new project console app

---
+++ tpmod5_1302223019/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tpmod5_1302223019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
