022833433bf4a9cb3da78fc692e44c2aeff2343f
purianagatha <purianagatha@gmail.com>
2024-03-18 04:55:58 +0700

    menambahkan class DataGeneric

--- tpmod5_1302223019/Program.cs
+++ tpmod5_1302223019/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace tpmod5_1302223019
+﻿public class HaloGeneric
 {
-    internal class Program
+    public void SapaUser<T>(T X)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Halo user " + X);
+    }
+}
+
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+    }
+}
+
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input nama: ");
+        String X = Console.ReadLine();
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(X);
+
+        DataGeneric<int> data = new DataGeneric<int>(1302223019);
+        data.PrintData();
+
     }
 }
